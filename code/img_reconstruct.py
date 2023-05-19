832a034122ad5cce05a2ea9c477ad33dc7ce2075
Matthew Thomas <mbthomas1124@gmail.com>
2023-05-19 01:01:44 -0400

    cleaned some code

--- code/img_reconstruct.py
+++ code/img_reconstruct.py
@@ -15,36 +15,20 @@ import matplotlib.pyplot as plt
 from typing import Tuple, Callable
 
 
-# use CUDA processors if available
-vers = torch.version.cuda
-print(vers)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-
-# prepare directories
-main_dir = os.getcwd()
-data_dir = os.path.join(main_dir, 'data', 'features', 'SIM_by_crystal')
-image_dir = os.path.join(main_dir, 'data', 'digit_image')
-
-# hyper-parameters:
-batch_size = 50
-learning_rate = 1e-3
-epochs = 512
-
-
 def adjust_img_path(path: str) -> str:
-    # modify image path to work in desktop version of google drive
+    # modify image paths to current directory location
+    main_dir = os.getcwd()
     return os.path.join(main_dir, *(path.split('/')[-4:]))
 
 
-def process_mat(directory: str, train: bool, test_files: list) -> Tuple[torch.Tensor, int, list, list]:
+def process_data(directory: str, train: bool, test_files: list) -> Tuple[torch.Tensor, int, list, list]:
     # prepare feature data and image paths
     image_paths = []
     digits = []
     raw_data = None
     num_features = 40
     clean = os.path.join(directory, 'clean_split')
+
     # iterate through the files in directory
     for filename in os.listdir(clean):
         file = os.path.join(clean, filename)
@@ -88,21 +72,18 @@ def process_mat(directory: str, train: bool, test_files: list) -> Tuple[torch.Te
     num_features = raw_data.shape[1]
     return raw_data, num_features, image_paths, digits
 
+
+
 # define data transformations
-
-
 def feature_transform(x: torch.Tensor):
     # standardizes the features of a given data point
     mean = x.mean()
     std = x.std()
     return x.sub(mean).div(std)
 
-
-convert_tensor = A.Compose([A.ToFloat(max_value=255), ToTensorV2()])
-
-
 def img_label_transform(y: str):
     # transforms an image path to a usable tensor
+    convert_tensor = A.Compose([A.ToFloat(max_value=255), ToTensorV2()])
     image = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
     try:
         label = convert_tensor(image=image)['image']
@@ -111,13 +92,12 @@ def img_label_transform(y: str):
         raise Exception(f"Image not found: {y}")
     return label.cuda()
 
-
 def digit_label_transform(y: str):
     return int(y)
 
+
+
 # create train and test Datasets and DataLoaders
-
-
 class ReconstructionData(Dataset):
     def __init__(self, raw_features: torch.Tensor, labels: list, feature_transform, label_transform: None):
         self.features = raw_features
@@ -185,15 +165,15 @@ class ClassifyNN(nn.Module):
         return logits
 
 
-def create_model(num_features: int, verbose: bool) -> nn.Module:
+def create_model(num_features: int, device: str, verbose: bool) -> nn.Module:
     model = ImgReconstructNN(num_features).to(device)
     if verbose:
         print(model)
     return model
 
+
+
 # Training loop
-
-
 def train_loop(dataloader: DataLoader, model: nn.Module, loss_fn, optimizer, print_loss: bool, loss_list: list) -> list:
     num_batches = len(dataloader)
     train_loss = 0
@@ -214,7 +194,7 @@ def train_loop(dataloader: DataLoader, model: nn.Module, loss_fn, optimizer, pri
     return loss_list
 
 
-def test_loop(dataloader, model, loss_fn, verbose: bool):
+def test_loop(dataloader, model, device, loss_fn, verbose: bool):
     print("Running Test Loop")
     num_batches = len(dataloader)
     test_loss = 0
@@ -225,23 +205,10 @@ def test_loop(dataloader, model, loss_fn, verbose: bool):
 
     with torch.no_grad():
         for batch, (X, y) in enumerate(dataloader):
-            # print(f"batch: {batch+1}")
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             test_ssim += ssim(pred, y).item()
             test_psnr += psnr(pred, y).item()
-
-            # # display 1 original and predicted image from each batch
-            # transform = transforms.ToPILImage()
-            # orig_data = y[0]
-            # orig_img = transform(orig_data)
-            # pred_data = pred[0]
-            # pred_img = transform(pred_data)
-            # print("original:")
-            # orig_img.show()
-            # print("predicted:")
-            # pred_img.show()
-            # print()
 
     test_loss /= num_batches
     test_psnr /= num_batches
@@ -276,120 +243,122 @@ def plot_loss(losses: list) -> None:
     plt.show()  # display the graph
 
 
-def run(directory: str, test_files, batch_size, loss_fn, learning_rate, epochs: int):
+def run(directory: str, device: str, test_files, batch_size, loss_fn, learning_rate, epochs: int):
     print("PART 1")
-    train_data, num_features, train_image_paths, _ = process_mat(
-        directory, True, None)
+    train_data, num_features, train_image_paths, _ = process_data(
+                                                                 directory, True, None)
     print("PART 2")
-    test_data, num_features, test_image_paths, _ = process_mat(
-        directory, False, None)
+    test_data, num_features, test_image_paths, _ = process_data(
+                                                               directory, False, None)
     print("PART 3")
     train_dataloader, test_dataloader = prep_data(
         train_data, train_image_paths, test_data, test_image_paths, img_label_transform, batch_size)
     print("PART 4")
-    model = create_model(num_features, True)
+    model = create_model(num_features, device, True)
     print("PART 5")
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     print("PART 6")
     train_loss = train_model(train_dataloader, model,
                              loss_fn, optimizer, epochs)
     print("PART 7")
-    # plot_loss(train_loss)
+    plot_loss(train_loss)
     print("PART 8")
     model, test_loss, test_psnr, test_ssim = test_loop(
-        test_dataloader, model, loss_fn, True)
+        test_dataloader, model, device, loss_fn, True)
     return model, train_loss, test_loss, test_psnr, test_ssim
-
-
-l1 = nn.L1Loss()
 
 
 class L1_SSIM_loss(MS_SSIM):
     def forward(self, x, y):
+        l1 = nn.L1Loss()
         return (0.5 * (1. - super().forward(x, y))) + (0.5 * l1.forward(x, y))
 
 
-loss_fn = L1_SSIM_loss(window_size=2, n_channels=1).cuda()
-# sample_dir = f'{data_dir}/original_EXP_data/with_noise/ChangeCrystalPosition_DATA/shiftCrystal40mm_SNR17(s10)_1-200samples_40LG_10loop_Oct12'
-# run(sample_dir, test_size, batch_size, loss_fn, learning_rate, epochs)
-
-# 64 Epoch piqa
-# Avg batch loss: 0.084998
-# Avg batch PSNR: 15.341266
-# Avg batch SSIM: 0.573755
-
-# 128 Epoch piqa
-# Avg batch loss: 0.063156
-# Avg batch PSNR: 16.070961
-# Avg batch SSIM: 0.636266
-
-# 256 epoch 1/2 SSIM 1/2 MAE
-# Avg batch loss: 0.057746
-# Avg batch PSNR: 17.995046
-# Avg batch SSIM: 0.764595
-crystals = ['0.8f', '27mm']
-for crystal in crystals:
-    sample_dir = os.path.join(data_dir, crystal)
-    model, train_loss, test_loss, test_psnr, test_ssim = run(sample_dir, None, batch_size, loss_fn, learning_rate, epochs)
-    filename = os.path.join(sample_dir, 'model.pt')
-    torch.save(model.state_dict(), filename)
-    filename = os.path.join(sample_dir, 'train_loss.pt')
-    torch.save(train_loss, filename)
-    filename = os.path.join(sample_dir, 'test_loss.pt')
-    torch.save(test_loss, filename)
-    filename = os.path.join(sample_dir, 'test_psnr.pt')
-    torch.save(test_psnr, filename)
-    filename = os.path.join(sample_dir, 'test_ssim.pt')
-    torch.save(test_ssim, filename)
-
-
-def test_run(directory: str, test_files, batch_size, loss_fn, learning_rate, epochs: int):
-    train_data, num_features, train_image_paths, _ = process_mat(
-        directory, True, test_files)
-    test_data, num_features, test_image_paths, _ = process_mat(
-        directory, False, test_files)
+def test_run( directory: str, device: str, test_files, batch_size, loss_fn, learning_rate, epochs: int):
+    train_data, num_features, train_image_paths, _ = process_data(
+                                                                 directory, True, test_files)
+    test_data, num_features, test_image_paths, _ = process_data(
+                                                               directory, False, test_files)
     _, test_dataloader = prep_data(
         train_data, train_image_paths, test_data, test_image_paths, img_label_transform, batch_size)
-    model = create_model(num_features, False)
+    model = create_model(num_features, device, False)
     filename = os.path.join(directory, 'model.pt')
     model.load_state_dict(torch.load(filename))
     model, test_loss, test_psnr, test_ssim = test_loop(
-        test_dataloader, model, loss_fn, False)
+        test_dataloader, model, device, loss_fn, False)
     return model, test_loss, test_psnr, test_ssim
 
 
-results = {}
-results['crystal_position'] = []
-results['filename'] = []
-results['test_loss'] = []
-results['test_psnr'] = []
-results['test_ssim'] = []
+def main():
+    # use CUDA processors if available
+    vers = torch.version.cuda
+    print(vers)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
 
-for crystal in crystals:
-    sample_dir = os.path.join(data_dir, crystal)
-    sample_clean = os.path.join(sample_dir, 'clean_split')
-    # iterate through the files in directory
-    for filename in os.listdir(sample_clean):
-        file = os.path.join(sample_clean, filename)
-        # checking if it is a file
-        if os.path.isfile(file):
-            if (filename[-7:] == 'test.pt'):
-                try:
-                    model, test_loss, test_psnr, test_ssim = test_run(
-                        sample_dir, [filename], batch_size, loss_fn, learning_rate, epochs)
-                    results['crystal_position'].append(crystal)
-                    results['filename'].append(filename)
-                    results['test_loss'].append(test_loss)
-                    results['test_psnr'].append(test_psnr)
-                    results['test_ssim'].append(test_ssim)
-                except:
+    # prepare directories
+    main_dir = os.getcwd()
+    data_dir = os.path.join(main_dir, 'data', 'features', 'SIM_by_crystal')
+
+    # hyper-parameters:
+    batch_size = 50
+    learning_rate = 1e-3
+    epochs = 512
+
+    loss_fn = L1_SSIM_loss(window_size=2, n_channels=1).cuda()
+
+    crystals = ['0.8f', '27mm']
+    for crystal in crystals:
+        sample_dir = os.path.join(data_dir, crystal)
+        model, train_loss, test_loss, test_psnr, test_ssim = run(
+            sample_dir, device, None, batch_size, loss_fn, learning_rate, epochs)
+        filename = os.path.join(sample_dir, 'model.pt')
+        torch.save(model.state_dict(), filename)
+        filename = os.path.join(sample_dir, 'train_loss.pt')
+        torch.save(train_loss, filename)
+        filename = os.path.join(sample_dir, 'test_loss.pt')
+        torch.save(test_loss, filename)
+        filename = os.path.join(sample_dir, 'test_psnr.pt')
+        torch.save(test_psnr, filename)
+        filename = os.path.join(sample_dir, 'test_ssim.pt')
+        torch.save(test_ssim, filename)
+
+    results = {}
+    results['crystal_position'] = []
+    results['filename'] = []
+    results['test_loss'] = []
+    results['test_psnr'] = []
+    results['test_ssim'] = []
+
+    for crystal in crystals:
+        sample_dir = os.path.join(data_dir, crystal)
+        sample_clean = os.path.join(sample_dir, 'clean_split')
+        # iterate through the files in directory
+        for filename in os.listdir(sample_clean):
+            file = os.path.join(sample_clean, filename)
+            # checking if it is a file
+            if os.path.isfile(file):
+                if (filename[-7:] == 'test.pt'):
+                    try:
+                        model, test_loss, test_psnr, test_ssim = test_run(
+                                                                          sample_dir, device, [filename], batch_size, loss_fn, learning_rate, epochs)
+                        results['crystal_position'].append(crystal)
+                        results['filename'].append(filename)
+                        results['test_loss'].append(test_loss)
+                        results['test_psnr'].append(test_psnr)
+                        results['test_ssim'].append(test_ssim)
+                    except:
+                        continue
+                else:
                     continue
             else:
-                continue
-        else:
-            print(file)
-            raise Exception("this is not a file")
+                print(file)
+                raise Exception("this is not a file")
 
-results = pd.DataFrame.from_dict(results)
-filename = os.path.join(main_dir, 'data', 'sim_results.csv')
-results.to_csv(filename, index=False)
+    results = pd.DataFrame.from_dict(results)
+    filename = os.path.join(main_dir, 'data', 'sim_results.csv')
+    results.to_csv(filename, index=False)
+
+
+if __name__ == "__main__":
+    main()
